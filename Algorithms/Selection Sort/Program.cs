c134d6d83321a71300226a76a596ca0f35f112c8
Chocoboskull <Inkedzelda@gmail.com>
2022-04-12 23:34:41 +0100

    Selection Sort - VSC

---
+++ Algorithms/Selection Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SelectionSort;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var array = new int[] { 37, 75, 94, 66, 120, 4, 2 };
+        var expected = new int[] { 2, 4, 37, 66, 75, 94, 120 };
+        
+        Console.WriteLine("Unsorted: ");
+        foreach(int i in array)
+        {
+            Console.Write(i + " ");
+        }
+
+        var sortFunction = new Selection();
+        sortFunction.IntArray = array;
+        var sortedArray = sortFunction.SortArray();
+     
+        Console.WriteLine("\nSorted: ");
+        foreach(int i in sortedArray)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nSorted = Expected : " + sortedArray.SequenceEqual(expected));
+    }
+}
