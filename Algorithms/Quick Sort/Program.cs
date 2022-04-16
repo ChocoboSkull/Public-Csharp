622bf361ea675c7b0fb7ade1d7877400aaf4e305
Chocoboskull <Inkedzelda@gmail.com>
2022-04-16 19:31:41 +0100

    Quick Sort - VSC

---
+++ Algorithms/Quick Sort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QuickSort;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var array = new int[] { 37, 75, 94, 66, 120, 4, 2 };
+        var expected = new int[] { 2, 4, 37, 66, 75, 94, 120 };
+
+        Console.WriteLine("Unsorted: ");
+        foreach(int i in array)
+        {
+            Console.Write(i + " ");
+        }
+
+        var sortFunction = new Quick();
+        var sortedArray = sortFunction.SortArray(array, 0, array.Length - 1);
+     
+        Console.WriteLine("\nSorted: ");
+        foreach(int i in sortedArray)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nSorted = Expected : " + sortedArray.SequenceEqual(expected));
+    }
+}
